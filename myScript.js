06ef031070ea20e0cfd5577f63a19b8d3250bee7
= <=>
2023-03-26 02:58:36 -0700

    Completed website updates including adding resume text, styling changes, and implementing close button functionality.
    Completed website updates including adding resume text, styling changes, and implementing close button functionality.

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    alert("know more about me");
+}
+
+function openResume() {
+    document.getElementById("resume-text").style.display = "block";
+}
+
+function closeResume() {
+    var resumeText = document.getElementById("resume-text");
+    resumeText.style.display = "none";
+  }
